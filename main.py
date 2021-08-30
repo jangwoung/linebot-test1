99281d43e3c07101e02af7682e3cf792ee2156da
jangwoung <masao31536@gmail.com>
2021-08-30 19:08:46 +0900

    test5-9

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import os
-import random
 
 from flask import Flask, request, abort
 from linebot import (
@@ -22,14 +21,11 @@ handler = WebhookHandler(LINE_CHANNEL_SECRET)
 
 @app.route("/callback", methods=['POST'])
 def callback():
-    # get X-Line-Signature header value
     signature = request.headers['X-Line-Signature']
 
-    # get request body as text
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
 
-    # handle webhook body
     try:
         handler.handle(body, signature)
     except InvalidSignatureError:
@@ -39,58 +35,25 @@ def callback():
 
 
 @handler.add(MessageEvent, message=TextMessage)
-def handle_message(event):
-    """
-    UserｓのActivityによって条件分岐。
-    noneQuestionがデフォルト→メニューを表示。
-    waitQestionが質問待ち状態→次に入力されたテキストが質問になる。
-    """
-    UserID = event.source.user_id
-    text = event.message.text
-    activity = users_DB.checkActivity(UserID)
+def response_message(event):
 
-    if activity == "noneQuestion":
-        line_bot_api.reply_message(
-            event.reply_token,
-            TextSendMessage(
-                text='メニュー',
-                quick_reply=QuickReply(
-                    items=[
-                        QuickReplyButton(
-                            action=PostbackAction(
-                                label="異性に質問してみる", data="異性に質問してみる")
-                        ),
-                        QuickReplyButton(
-                            action=PostbackAction(
-                                label="誰かの質問に答える", data="誰かの質問に答える")
-                        ),
-                    ])))
+    if event.message.text == "Todo":
+
+        language_list = ["Ruby", "Python", "PHP", "Java", "C"]
+
+        items = [QuickReplyButton(action=MessageAction(
+            label=f"{language}", text=f"{language}が好き")) for language in language_list]
+
+        messages = TextSendMessage(text="どの言語が好きですか？",
+                                   quick_reply=QuickReply(items=items))
+
+        line_bot_api.reply_message(event.reply_token, messages=messages)
+
     else:
-        QuestionKey = activity
-        questions.putAnswer(UserID, text, QuestionKey)
-        users_DB.changeActivity(UserID, "waitAnswer")
-
+        message = event.message.text
         line_bot_api.reply_message(
             event.reply_token,
-            TextSendMessage(
-                text=text + "をあなたの回答として投稿しました",
-                quick_reply=QuickReply(
-                    items=[
-                            QuickReplyButton(
-                                action=PostbackAction(
-                                    label="自分への回答を確認する", data="回答を確認する")
-                            ),
-                        QuickReplyButton(
-                                action=PostbackAction(
-                                    label="誰かの質問に答える", data="誰かの質問に答える")
-                            ),
-                        QuickReplyButton(
-                                action=PostbackAction(
-                                    label="質問をする", data="異性に質問してみる")
-                            ),
-                    ])))
-
-    return "ok"
+            TextSendMessage(text=message))
 
 
 if __name__ == "__main__":
