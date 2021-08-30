324d41ee4982216f77274f209fad8e0f313e0974
jangwoung <masao31536@gmail.com>
2021-08-30 15:34:24 +0900

    test2-20

--- main.py
+++ main.py
@@ -45,7 +45,7 @@ def handle_message(event):
     # 基本的にここにコードを書いていきます。
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text='[' + event.message.text + ']'))
+        TextSendMessage(text='3' + event.message.text + '4'))
 
 
 if __name__ == "__main__":
