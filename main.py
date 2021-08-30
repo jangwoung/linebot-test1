f4d3a75cdaf15bca6b5038e53408a943d0d98bb4
jangwoung <masao31536@gmail.com>
2021-08-30 19:02:50 +0900

    test5-8

--- main.py
+++ main.py
@@ -40,28 +40,57 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+    """
+    UserｓのActivityによって条件分岐。
+    noneQuestionがデフォルト→メニューを表示。
+    waitQestionが質問待ち状態→次に入力されたテキストが質問になる。
+    """
+    UserID = event.source.user_id
+    text = event.message.text
+    activity = users_DB.checkActivity(UserID)
 
-    # 何を行う？
-    message = event.message.text
-
-    if message == 'todo':
-
-        language_list = ["Ruby", "Python", "PHP", "Java", "C"]
-        items = [QuickReplyButton(action=MessageAction(
-            label=f"{language}", text=f"{language}が好き")) for language in language_list]
-
+    if activity == "noneQuestion":
         line_bot_api.reply_message(
             event.reply_token,
-            messages=TextSendMessage(text="どの言語が好きですか？",
-                                     quick_reply=QuickReply(items=items))
-            line_bot_api.reply_message(event.reply_token, messages=messages)
-        )
-
+            TextSendMessage(
+                text='メニュー',
+                quick_reply=QuickReply(
+                    items=[
+                        QuickReplyButton(
+                            action=PostbackAction(
+                                label="異性に質問してみる", data="異性に質問してみる")
+                        ),
+                        QuickReplyButton(
+                            action=PostbackAction(
+                                label="誰かの質問に答える", data="誰かの質問に答える")
+                        ),
+                    ])))
     else:
+        QuestionKey = activity
+        questions.putAnswer(UserID, text, QuestionKey)
+        users_DB.changeActivity(UserID, "waitAnswer")
+
         line_bot_api.reply_message(
             event.reply_token,
-            TextSendMessage(text=message)
-        )
+            TextSendMessage(
+                text=text + "をあなたの回答として投稿しました",
+                quick_reply=QuickReply(
+                    items=[
+                            QuickReplyButton(
+                                action=PostbackAction(
+                                    label="自分への回答を確認する", data="回答を確認する")
+                            ),
+                        QuickReplyButton(
+                                action=PostbackAction(
+                                    label="誰かの質問に答える", data="誰かの質問に答える")
+                            ),
+                        QuickReplyButton(
+                                action=PostbackAction(
+                                    label="質問をする", data="異性に質問してみる")
+                            ),
+                    ])))
+
+    return "ok"
 
 
 if __name__ == "__main__":
