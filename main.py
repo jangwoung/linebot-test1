a190f5714b746721cd3ddc0ffdbbb6e4f4769e06
jangwoung <masao31536@gmail.com>
2021-08-30 16:21:32 +0900

    test3-7

--- main.py
+++ main.py
@@ -63,10 +63,6 @@ def callback():
 def handle_message(event):
     # 基本的にここにコードを書いていきます。event.message.text
     if event.message.text == "Hi":
-        line_bot_api.reply_message(
-            event.reply_token,
-            TextSendMessage(text='Hello')
-        )
 
         line_bot_api.reply_message(
             event.reply_token,
