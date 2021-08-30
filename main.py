94ea7504794d5047124afab5f1a5e4ebc95d3278
jangwoung <masao31536@gmail.com>
2021-08-30 15:23:27 +0900

    test2-16

--- main.py
+++ main.py
@@ -43,10 +43,9 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     # 基本的にここにコードを書いていきます。
-    message = event.message.text
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text='[' + message + ']'))
+        TextSendMessage(text='[' + event.message.text + ']'))
 
 
 if __name__ == "__main__":
