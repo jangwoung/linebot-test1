6c82259b4add9f3a7c2c01febf7f8c2e7281a645
jangwoung <masao31536@gmail.com>
2021-08-30 20:42:40 +0900

    test5-14

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import os
+import random
+import gspread
 
+
+from oauth2client.service_account import ServiceAccountCredentials
 from flask import Flask, request, abort
 from linebot import (
     LineBotApi, WebhookHandler
@@ -9,6 +13,17 @@ from linebot.exceptions import (
 )
 from linebot.models import (
     MessageEvent, TextMessage, QuickReplyButton, MessageAction, QuickReply, TextSendMessage, ImageSendMessage, VideoSendMessage, StickerSendMessage, AudioSendMessage)
+
+scope = ['https://spreadsheets.google.com/feeds',
+         'https://www.googleapis.com/auth/drive']
+
+credentials = ServiceAccountCredentials.from_json_keyfile_name(
+    'line-bot-test-324508-8fb79546f3b4.json', scope)
+gc = gspread.authorize(credentials)
+wks = gc.open('gspreadサンプル').sheet1
+
+wks.update_acell('A1', 'Hello World!')
+print(wks.acell('A1'))
 
 app = Flask(__name__)
 
@@ -50,6 +65,7 @@ def response_message(event):
         line_bot_api.reply_message(event.reply_token, messages=messages)
 
     elif event.message.text == "I want make Today's todo list":
+
         line_bot_api.reply_message(
             event.reply_token,
             TextSendMessage(text="No.1"))
@@ -70,8 +86,6 @@ def response_message(event):
             event.reply_token,
             TextSendMessage(text="No.5"))
         e = event.message.text
-
-        todolist = [a, b, c, d, e]
 
         for x in todolist:
             line_bot_api.reply_message(
