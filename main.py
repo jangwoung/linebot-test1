ae24452ea03ffecfb57b98a80c0bc6698f1af6e9
jangwoung <masao31536@gmail.com>
2021-08-31 12:09:27 +0900

    test8-6

--- main.py
+++ main.py
@@ -33,19 +33,20 @@ def callback():
 @ handler.add(MessageEvent, message=TextMessage)
 def response_message(event):
     if event.message.text == "Todo":
-        select_list = ["make", "check", "finish"]
+        select_list = ["Set", "check", "finish"]
         items = [QuickReplyButton(action=MessageAction(
             label=f"{select}", text=f"I want {select} Today's todo list")) for select in select_list]
         msg1 = TextSendMessage(text="What do you want to do?",
                                quick_reply=QuickReply(items=items))
         line_bot_api.reply_message(event.reply_token, messages=msg1)
-    elif event.message.text == "I want make Today's todo list":
+    elif event.message.text == "I want set Today's todo list":
         setting_list = ["No.1", "No.2", "No.3"]
         items = [QuickReplyButton(action=PostbackAction(
             label=f"{setting}", data=f"{setting}")) for setting in setting_list]
         msg2 = TextSendMessage(text="select todo number!",
                                quick_reply=QuickReply(items=items))
         line_bot_api.reply_message(event.reply_token, messages=msg2)
+
     elif event.message.text == "I want check Today's todo list":
         line_bot_api.reply_message(
             event.reply_token,
@@ -63,11 +64,12 @@ def response_message(event):
 @handler.add(PostbackEvent)
 def handle_postback(event):
     if event.postback.data == 'No.1':
-
-        line_bot_api.reply_message(
-            event.reply_token,
-            TextSendMessage(
-                text='Hi'))
+        select_list = ["study", "exercise", "reading", "sleep", "shopping"]
+        items = [QuickReplyButton(action=MessageAction(
+            label=f"{select}", text=f"Let's {select} Today!")) for select in select_list]
+        msg1 = TextSendMessage(text="{select}",
+                               quick_reply=QuickReply(items=items))
+        line_bot_api.reply_message(event.reply_token, messages=msg1)
 
 
 if __name__ == "__main__":
