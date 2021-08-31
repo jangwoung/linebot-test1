f2ce45974a7a678f35486359656da13d55b8a275
jangwoung <masao31536@gmail.com>
2021-08-31 09:22:54 +0900

    test7-20

--- main.py
+++ main.py
@@ -42,22 +42,26 @@ def callback():
 def response_message(event):
 
     if event.message.text == "Todo":
-        language_list = ["make", "check", "finish"]
+        select_list = ["make", "check", "finish"]
 
         items = [QuickReplyButton(action=MessageAction(
-            label=f"{language}", text=f"I want {language} Today's todo list")) for language in language_list]
+            label=f"{select}", text=f"I want {select} Today's todo list")) for select in select_list]
 
-        messages = TextSendMessage(text="What do you want to do?",
-                                   quick_reply=QuickReply(items=items))
+        msg1 = TextSendMessage(text="What do you want to do?",
+                               quick_reply=QuickReply(items=items))
 
-        line_bot_api.reply_message(event.reply_token, messages=messages)
+        line_bot_api.reply_message(event.reply_token, messages=msg1)
 
     elif event.message.text == "I want make Today's todo list":
+        setting_list = ["No.1", "No.2", "No.3"]
 
-        line_bot_api.reply_message(
-            event.reply_token,
-            TextSendMessage(text="Please enter what you want to do Today!"))
-        a = event.message.text
+        items = [QuickReplyButton(action=MessageAction(
+            label=f"{setting}", text=f"I want {setting} Today's todo list")) for setting in setting_list]
+
+        msg2 = TextSendMessage(text="What do you want to do?",
+                               quick_reply=QuickReply(items=items))
+
+        line_bot_api.reply_message(event.reply_token, messages=msg2)
 
     elif event.message.text == "I want check Today's todo list":
         line_bot_api.reply_message(
@@ -74,78 +78,6 @@ def response_message(event):
         line_bot_api.reply_message(
             event.reply_token,
             TextSendMessage(text="Please enter\"Todo\""))
-
-
-if __name__ == "__main__":
-    port = int(os.getenv("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
-
-
-app = Flask(__name__)
-num = 0
-
-LINE_CHANNEL_ACCESS_TOKEN = os.environ["LINE_CHANNEL_ACCESS_TOKEN"]
-LINE_CHANNEL_SECRET = os.environ["LINE_CHANNEL_SECRET"]
-
-line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
-handler = WebhookHandler(LINE_CHANNEL_SECRET)
-
-
-@app.route("/callback", methods=['POST'])
-def callback():
-    signature = request.headers['X-Line-Signature']
-
-    body = request.get_data(as_text=True)
-    app.logger.info("Request body: " + body)
-
-    try:
-        handler.handle(body, signature)
-    except InvalidSignatureError:
-        abort(400)
-
-    return 'OK'
-
-
-@handler.add(MessageEvent, message=TextMessage)
-def response_message(event):
-    if event.message.text == "Todo":
-        language_list = ["make", "check", "finish"]
-
-        items = [QuickReplyButton(action=MessageAction(
-            label=f"{language}", text=f"I want {language} Today's todo list")) for language in language_list]
-
-        messages = TextSendMessage(text="What do you want to do?",
-                                   quick_reply=QuickReply(items=items))
-
-        line_bot_api.reply_message(event.reply_token, messages=messages)
-
-    elif event.message.text == "I want make Today's todo list":
-
-        language_list = ["make", "check", "finish"]
-
-        items = [QuickReplyButton(action=MessageAction(
-            label=f"{language}", text=f"I want {language} Today's todo list")) for language in language_list]
-
-        messages = TextSendMessage(text="What do you want to do?",
-                                   quick_reply=QuickReply(items=items))
-
-        line_bot_api.reply_message(event.reply_token, messages=messages)
-
-    elif event.message.text == "I want check Today's todo list":
-        line_bot_api.reply_message(
-            event.reply_token,
-            TextSendMessage(text="Todo"))
-
-    elif event.message.text == "I want finish Today's todo list":
-        line_bot_api.reply_message(
-            event.reply_token,
-            TextSendMessage(text="Todo"))
-
-    else:
-        message = event.message.text
-        line_bot_api.reply_message(
-            event.reply_token,
-            TextSendMessage(text=message))
 
 
 if __name__ == "__main__":
